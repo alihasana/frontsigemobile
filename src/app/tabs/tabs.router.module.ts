52f921c75168ea32041bb3077b3a19f6e288cde7
alihasana <hasana.ali@gmail.com>
2019-03-04 07:19:42 +0100

    untracked file added in application

--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -1,57 +1,42 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { TabsPage } from './tabs.page';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { TabsPage } from "./tabs.page";
 
 const routes: Routes = [
   {
-    path: 'tabs',
+    path: "tabs",
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        children: [
-          {
-            path: '',
-            loadChildren: '../tab1/tab1.module#Tab1PageModule'
-          }
-        ]
+        path: "tab1",
+        children: [{ path: "", loadChildren: "../tab1/tab1.module#Tab1PageModule"}]
       },
       {
-        path: 'tab2',
-        children: [
-          {
-            path: '',
-            loadChildren: '../tab2/tab2.module#Tab2PageModule'
-          }
-        ]
+        path: "tab2",
+        children: [{ path: "", loadChildren: "../tab2/tab2.module#Tab2PageModule"}]
       },
       {
-        path: 'tab3',
-        children: [
-          {
-            path: '',
-            loadChildren: '../tab3/tab3.module#Tab3PageModule'
-          }
-        ]
+        path: "tab3",
+        children: [{path: "", loadChildren: "../tab3/tab3.module#Tab3PageModule"}        ]
       },
-      {
-        path: '',
-        redirectTo: '/tabs/tab1',
-        pathMatch: 'full'
-      }
+      {path: "", redirectTo: "/tabs/tab1", pathMatch: "full"}
     ]
   },
   {
-    path: '',
-    redirectTo: '/tabs/tab1',
-    pathMatch: 'full'
+    path: 'home',
+    loadChildren: '../home/home.module#HomePageModule'
+  },
+  {
+    path: 'list',
+    loadChildren: '../list/list.module#ListPageModule'
+  },
+  {
+    path: "", redirectTo: "/tabs/tab1", pathMatch: "full"
   }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forChild(routes)
-  ],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class TabsPageRoutingModule {}
